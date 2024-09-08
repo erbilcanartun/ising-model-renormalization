600f4a4efdc1d5638f14e1ff9ebfb751303cd08c
Erbil Can Artun <artunerbilcan@gmail.com>
2024-09-08 19:15:25 +0300

    Refactor renormalization.py for improved readability and maintainability

--- renormalization.py
+++ renormalization.py
@@ -1,58 +1,117 @@
 import numpy as np
+from scipy.misc import derivative
 
-def partial_derivative(function, variable=0, point=[]):
-    arguments = point[:]
-    def wraps(x):
-        arguments[variable] = x
-        return function(*arguments)
+class RenormalizationGroup:
 
-def lnR1(j, h):
+    def __init__(self):
+    
+        self.b = 2 #decimation
+        self.d = 1 #dimension
 
-    e1 = 2*j + 4*h
-    e2 = -2*j
-    emax = np.amax([e1, e2])
+        
+    def _pderivative(self, function, variable=0, point=[]):
+        arguments = point[:]
+        def wraps(x):
+            arguments[variable] = x
+            return function(*arguments)
+        return derivative(wraps, point[variable], dx=np.sqrt(np.finfo(float).eps))
+    
+    def _lnR1(self, j, h):
+    
+        e1 = 2*j + 4*h
+        e2 = -2*j
+        emax = np.amax([e1, e2])
+    
+        return emax + np.log(np.exp(e1 - emax) + np.exp(e2 - emax))
+    
+    def _lnR2(self, j, h):
+    
+        e1 = -2*j
+        e2 = 2*j - 4*h
+        emax = np.amax([e1, e2])
+    
+        return emax + np.log(np.exp(e1 - emax) + np.exp(e2 - emax))
+    
+    def _lnR3(self, j, h):
+    
+        e1 = 2*h
+        e2 = -2*h
+        emax = np.amax([e1, e2])
+    
+        return emax + np.log(np.exp(e1 - emax) + np.exp(e2 - emax))
+    
+    def J(self, j, h):
+        return (1/4) * (self._lnR1(j, h) + self._lnR2(j, h) - 2*self._lnR3(j, h))
+    
+    def H(self, j, h):
+        return (1/4) * (self._lnR1(j, h) - self._lnR2(j, h))
+    
+    def G(self, j, h):
+        return (1/4) * (self._lnR1(j, h) + self._lnR2(j, h) + 2*self._lnR3(j, h))
+    
+    def recursion_matrix(self, j, h):
 
-    return emax + np.log(np.exp(e1 - emax) + np.exp(e2 - emax))
+        m = self.b ** self.d
+    
+        X = self._pderivative(self.G, 0, [j, h])
+        Y = self._pderivative(self.J, 0, [j, h])
+        Z = self._pderivative(self.H, 0, [j, h])
+    
+        A = self._pderivative(self.G, 1, [j, h])
+        B = self._pderivative(self.J, 1, [j, h])
+        C = self._pderivative(self.H, 1, [j, h])
+    
+        return np.array([[m, X, A],
+                         [0, Y, B],
+                         [0, Z, C]])
 
-def lnR2(j, h):
+    def flow(self, initial_conditions, n):
 
-    e1 = -2*j
-    e2 = 2*j - 4*h
-    emax = np.amax([e1, e2])
-
-    return emax + np.log(np.exp(e1 - emax) + np.exp(e2 - emax))
-
-def lnR3(j, h):
-
-    e1 = 2*h
-    e2 = -2*h
-    emax = np.amax([e1, e2])
-
-    return emax + np.log(np.exp(e1 - emax) + np.exp(e2 - emax))
-
-def J(j, h):
-    return (1/4) * (lnR1(j, h) + lnR2(j, h) - 2*lnR3(j, h))
-
-def H(j, h):
-    return (1/4) * (lnR1(j, h) - lnR2(j, h))
-
-def G(j, h):
-    return (1/4) * (lnR1(j, h) + lnR2(j, h) + 2*lnR3(j, h))
-
-def recursion_matrix(j, h):
-
-    decimation, dimension = 2, 1
-    m = decimation**dimension
-
-    X = partial_derivative(G, 0, [j,h])
-    Y = partial_derivative(J, 0, [j,h])
-    Z = partial_derivative(H, 0, [j,h])
-
-    K = partial_derivative(G, 1, [j,h])
-    L = partial_derivative(J, 1, [j,h])
-    M = partial_derivative(H, 1, [j,h])
-
-    return [[m, X,   K],
-            [0,     Y,   L],
-            [0,     Z,   M]]
-
+        [j, h] = initial_conditions
+        
+        print("k,   J         H\n------------------")
+        print(0, "  ", j, "    ", h)
+        
+        for i in range(n):
+                
+            j, h = self.J(j, h), self.H(j, h)
+            print(i+1, "  ", j, "    ", h)
+    
+    
+    def densities(self, field=0):
+    # Densities under fixed H
+        
+            temp_list = []
+            M_results = []
+        
+            j_initial = 0.01
+        
+            while j_initial < 30:
+            
+                h = field
+                j = j_initial
+                temp_list.append(1 / j)
+                
+                if h > 0:
+                    Mn = [1, 0, 1]
+                    
+                if h < 0:
+                    Mn = [1, 0, -1]
+                
+                if h == 0:
+                    Mn = [1, 0, 0]
+                    
+                U = np.identity(3)
+            
+                for i in range(15):
+                    
+                    U = (self.b ** (-self.d)) * np.dot(self.recursion_matrix(j, h), U)
+                    j, h = self.J(j, h), self.H(j, h)
+                    
+                M = np.dot(Mn, U)
+            
+                M_results.append(M)
+            
+                j_initial = j_initial + 0.01
+        
+            return np.array(temp_list), np.array(M_results)
